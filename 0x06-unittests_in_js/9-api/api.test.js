7bdc01cc07038acf96d35b4807f206c866ce12cf
eyobsis <ehtube2020@gmail.com>
2024-05-02 15:04:10 -0700

    commit

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,5 +1,5 @@
-const request = require('request');
-const expect = require('chai').expect;
+import request from'request';
+import { expect } from 'chai';
 
 describe('Index page', () => {
   it('should return a 200 status code', (done) => {
