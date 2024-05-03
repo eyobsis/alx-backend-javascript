6935a601e729282f9b4af91c3d4af39a0618ef06
eyobsis <ehtube2020@gmail.com>
2024-05-03 16:36:20 -0700

    commit task5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,67 +1,29 @@
 const http = require('http');
-const fs = require('fs').promises;
-const url = require('url');
+const countStudents = require('./3-read_file_async');
 
-const PORT = 1245;
-const databaseFile = process.argv[2];
-
-const parseCSV = async (data) => {
-  const lines = data.split('\n').filter((line) => line.trim() !== '');
-  const students = {
-    CS: [],
-    SWE: [],
-  };
-
-  lines.forEach(line => {
-    const [name, field] = line.split(',');
-    if (field && (field.trim() === 'CS' || field.trim() === 'SWE')) {
-      students[field.trim()].push(name.trim());
-    }
-  });
-
-  return {
-    total: lines.length,
-    CS: {
-      count: students.CS.length,
-      list: students.CS.join(', '),
-    },
-    SWE: {
-      count: students.SWE.length,
-      list: students.SWE.join(', '),
-    },
-  };
-};
+const hostname = '127.0.0.1';
+const port = 1245;
 
 const app = http.createServer(async (req, res) => {
-  const reqUrl = url.parse(req.url, true);
-
-  if (reqUrl.pathname === '/') {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello Holberton School!');
-    res.end();
-  } else if (reqUrl.pathname === '/students') {
-    try {
-      const fileData = await fs.readFile(databaseFile, 'utf8');
-      const studentsData = await parseCSV(fileData);
-      const response = `This is the list of our students\nNumber of students: ${studentsData.total}\nNumber of students in CS: ${studentsData.CS.count}. List: ${studentsData.CS.list}\nNumber of students in SWE: ${studentsData.SWE.count}. List: ${studentsData.SWE.list}`;
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.write(response);
-      res.end();
-    } catch (error) {
-      res.writeHead(500, {'Content-Type': 'text/plain'});
-      res.write('Error reading file');
-      res.end();
-    }
-  } else {
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.write('Not Found');
-    res.end();
+  res.statusCode = 200;
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    let dbInfo = 'This is the list of our students\n';
+    await countStudents(process.argv[2])
+      .then((msg) => {
+        dbInfo += msg;
+        res.end(dbInfo);
+      })
+      .catch((err) => {
+        dbInfo += err.message;
+        res.end(dbInfo);
+      });
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
 });
 
 module.exports = app;
-
